53ad4e2562ffd6b043008c555ad3201a357b4e6c
Priyadharshini Arulanantham <priyadharshini.arulanantham@psiog.com>
2025-10-07 00:51:19 +0530

    display

---
+++ hangman/hangman/Views/ConsoleView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HangmanGameMVC.Views
+{
+    public class ConsoleView
+    {
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string ReadInput(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+    }
+}
