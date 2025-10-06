7d22a6690925418eaadc0956ee223766aca7fc93
Priyadharshini Arulanantham <priyadharshini.arulanantham@psiog.com>
2025-10-07 00:38:40 +0530

    interface repo

---
+++ hangman/hangman/Models/Word/IWordRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace HangmanGameMVC.Models.Word
+{
+    public interface IWordRepository
+    {
+        List<Word> GetAllWords();
+        void AddWord(Word word);
+        void UpdateWord(string oldWord, string newWord);
+        void DeleteWord(string word);
+        Word GetRandomWord();
+    }
+}
