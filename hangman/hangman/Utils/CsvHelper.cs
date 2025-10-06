f0a2ec7e4425a558292e8c2bd8796199ab234d68
Priyadharshini Arulanantham <priyadharshini.arulanantham@psiog.com>
2025-10-07 00:46:56 +0530

    csv file connection

---
+++ hangman/hangman/Utils/CsvHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace HangmanGameMVC.Utils { 
+    public static class CsvHelper
+    { public static List<string[]> ReadCsv(string path) 
+        { var lines = new List<string[]>(); 
+            if (!File.Exists(path)) return lines;
+            foreach (var line in File.ReadAllLines(path)) {
+                lines.Add(line.Split(','));
+            }
+            return lines;
+        }
+        public static void WriteCsv(string path, List<string[]> data) 
+        {
+            var lines = new List<string>();
+            foreach (var row in data)
+            { 
+                lines.Add(string.Join(",", row));
+            } File.WriteAllLines(path, lines); 
+        } 
+    } 
+}
