5af2229c21817b47666cb97cd5d21d5ea08faa62
Priyadharshini Arulanantham <priyadharshini.arulanantham@psiog.com>
2025-10-07 00:37:34 +0530

    word file

---
+++ hangman/hangman/Models/Word/Word.cs
@@ -0,0 +1,14 @@
+﻿namespace HangmanGameMVC.Models.Word
+{
+    public class Word
+    {
+        public string Text { get; set; }
+
+        public Word() { }
+
+        public Word(string text)
+        {
+            Text = text;
+        }
+    }
+}
