ad540ecefeedc3ef70a656cb1f9493a356fd04a4
Priyadharshini Arulanantham <priyadharshini.arulanantham@psiog.com>
2025-10-07 00:36:41 +0530

    word repo

---
+++ hangman/hangman/Models/Word/WordRepository.cs
@@ -0,0 +1,57 @@
+﻿using HangmanGameMVC.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HangmanGameMVC.Models.Word
+{
+    public class WordRepository : IWordRepository
+    {
+        private readonly string _filePath = "Data/words.csv";
+
+        public List<Word> GetAllWords()
+        {
+            var lines = CsvHelper.ReadCsv(_filePath);
+            return lines.Select(l => new Word(l[0])).ToList();
+        }
+
+        public void AddWord(Word word)
+        {
+            var words = CsvHelper.ReadCsv(_filePath);
+            if (!words.Any(w => w[0] == word.Text))
+            {
+                words.Add(new string[] { word.Text });
+                CsvHelper.WriteCsv(_filePath, words);
+            }
+        }
+
+        public void UpdateWord(string oldWord, string newWord)
+        {
+            var words = CsvHelper.ReadCsv(_filePath);
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (words[i][0] == oldWord)
+                {
+                    words[i][0] = newWord;
+                    break;
+                }
+            }
+            CsvHelper.WriteCsv(_filePath, words);
+        }
+
+        public void DeleteWord(string word)
+        {
+            var words = CsvHelper.ReadCsv(_filePath);
+            words.RemoveAll(w => w[0] == word);
+            CsvHelper.WriteCsv(_filePath, words);
+        }
+
+        public Word GetRandomWord()
+        {
+            var words = GetAllWords();
+            if (words.Count == 0) return null;
+            Random rnd = new Random();
+            return words[rnd.Next(words.Count)];
+        }
+    }
+}
