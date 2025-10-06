9a6daca48f1f3303b66aa45e78bffb92299f866e
Priyadharshini Arulanantham <priyadharshini.arulanantham@psiog.com>
2025-10-07 00:46:08 +0530

    hanging logic

---
+++ hangman/hangman/Models/Game/HangmanLogic.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace HangmanGameMVC.Models.Game
+{
+    public class HangmanLogic : IGameLogic
+    {
+        private GameState _state;
+        private bool _askPlayAgain;
+
+        public HangmanLogic(string word)
+        {
+            _state = new GameState(word.ToUpper());
+        }
+
+        public void StartGame()
+        {
+            _state.LetterIndex = 0;
+            _state.AttemptCount = 3;
+            _state.GameOver = false;
+            _state.WordCompleted = false;
+            _askPlayAgain = false;
+
+            for (int i = 0; i < _state.CurrentProgress.Length; i++)
+                _state.CurrentProgress[i] = '_';
+        }
+
+        public bool Guess(char letter)
+        {
+            if (_state.GameOver) return false;
+
+            letter = char.ToUpper(letter);
+            bool correct = false;
+
+            // Fill all occurrences of guessed letter
+            for (int i = 0; i < _state.WordToGuess.Length; i++)
+            {
+                if (_state.WordToGuess[i] == letter && _state.CurrentProgress[i] != letter)
+                {
+                    _state.CurrentProgress[i] = letter;
+                    correct = true;
+                }
+            }
+
+            if (correct)
+            {
+                _state.AttemptCount = 3; // reset attempts for next letter
+
+                // Word completed
+                if (!_state.CurrentProgress.Contains('_'))
+                {
+                    _state.WordCompleted = true;
+                    _state.GameOver = true;
+                    _askPlayAgain = true;
+
+                    // Show happy human
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nCongratulations! You guessed the word correctly!");
+                    Console.WriteLine(@"
+     \O/
+      |
+     / \
+  H A P P Y !");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                _state.AttemptCount--;
+
+                if (_state.AttemptCount <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; // Set red color
+                    Console.WriteLine(GetHangmanUI());
+                    Console.WriteLine("     YOU ARE HANGED!");
+                    Console.ResetColor(); // Reset back to default color
+
+                    _state.GameOver = true;
+                    _askPlayAgain = true;
+
+                    // Reset word progress for next round
+                    for (int i = 0; i < _state.CurrentProgress.Length; i++)
+                        _state.CurrentProgress[i] = '_';
+                    _state.LetterIndex = 0;
+                    _state.AttemptCount = 3;
+                }
+
+            }
+
+            return correct;
+        }
+
+        public bool ShouldAskPlayAgain() => _askPlayAgain;
+
+        public void AskPlayAgainPrompt()
+        {
+            Console.Write("\nDo you want to play again? (y/n): ");
+            string choice = Console.ReadLine()?.Trim().ToLower() ?? "n";
+
+            if (choice == "y")
+            {
+                Console.Clear();
+                Console.WriteLine("Starting a new round...");
+                StartGame();
+            }
+            else
+            {
+                Console.WriteLine("\nThanks for playing! Exiting...");
+                Environment.Exit(0);
+            }
+        }
+
+        public string GetHangmanUI()
+        {
+            int wrongGuess = 3 - _state.AttemptCount;
+
+            return wrongGuess switch
+            {
+                1 => @"
+  +---+
+  |   |
+  |
+  |
+  |
+  |
+=========",
+                2 => @"
+  +---+
+  |   |
+  |   O
+  |
+  |
+  |
+=========",
+                3 => @"
+  +---+
+  |   |
+  |   O
+  |  /|\
+  |  / \
+  |
+=========",
+                _ => ""
+            };
+        }
+
+        public string GetProgress() => string.Join(" ", _state.CurrentProgress);
+
+        public int GetAttempts() => _state.AttemptCount;
+
+        public bool IsGameOver() => _state.GameOver;
+
+        public bool IsWordCompleted() => _state.WordCompleted;
+    }
+}
