94d40c52905b0dd7695c67f14e3a00368eea787c
Priyadharshini Arulanantham <priyadharshini.arulanantham@psiog.com>
2025-10-07 00:31:13 +0530

    userdetails

---
+++ hangman/hangman/Models/User/User.cs
@@ -0,0 +1,18 @@
+﻿namespace HangmanGameMVC.Models.User
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } // "admin" or "user"
+
+        public User() { }
+
+        public User(string username, string password, string role)
+        {
+            Username = username;
+            Password = password;
+            Role = role;
+        }
+    }
+}
