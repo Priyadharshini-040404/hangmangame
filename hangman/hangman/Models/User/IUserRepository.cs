979527d791c55af76088cb081a4bb928c346212a
Priyadharshini Arulanantham <priyadharshini.arulanantham@psiog.com>
2025-10-07 00:34:17 +0530

    Repo linking

---
+++ hangman/hangman/Models/User/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace HangmanGameMVC.Models.User
+{
+    public interface IUserRepository
+    {
+        User GetUser(string username, string password);
+        bool AddUser(User user);
+        List<User> GetAllUsers();
+    }
+}
